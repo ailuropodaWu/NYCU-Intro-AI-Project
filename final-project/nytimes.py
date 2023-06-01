78c1b9ff33ebc46909d5efabee0f2664b438a5f3
Chuan-Yu Wu <patrick050292@gmail.com>
2023-06-02 00:20:01 +0800

    Update nytimes.py

--- final-project/nytimes.py
+++ final-project/nytimes.py
@@ -20,11 +20,11 @@ def get_data():
     
     pd.DataFrame(data).to_pickle('./nytimes-dataset/test.pkl')
 
-def get_train_data(data_path = './nytimes-dataset/nytimes.pkl', num_train_data = 100):
+def get_train_data(data_path = './nytimes-dataset/nytimes.pkl'):
 
     print('Access data from pickle')
     data = pd.read_pickle(data_path)
-    pick_rate = num_train_data / len(data)
+    pick_rate = 2 / 3
     print(f'Pick Rate : {pick_rate}')
 
     print('Pick the data to train')
@@ -32,17 +32,28 @@ def get_train_data(data_path = './nytimes-dataset/nytimes.pkl', num_train_data =
     for i, row in tqdm(data.iterrows()):
         if random.uniform(0,1) < pick_rate:
             train_data.append(row)
+        else:
+            test_data.append(row)
     train = pd.DataFrame(train_data)
+    test = pd.DataFrame(test_data)
     train = train.reset_index().drop(columns=['index'])
+    test = test.reset_index().drop(columns=['index'])
     print(f'Get {len(train)} Train Data')
+    print(f'Get {len(test)} Train Data')
 
+    train = process(train)
+    test = process(test)
+    return train, test
+
+def process(data):
+    
     print('Proccess the pub data')
     pub_year = []
     pub_month = []
     pub_day = []
     pub_weekday = []
     pub_time = []
-    for i in tqdm(train['pub_date']):
+    for i in tqdm(data['pub_date']):
         i = pd.to_datetime(i)
         pub_year.append(i.year)
         pub_month.append(i.month)
@@ -54,8 +65,8 @@ def get_train_data(data_path = './nytimes-dataset/nytimes.pkl', num_train_data =
     abstract = []
     abstract_len = []
     abstract_dup = []
-    dup = train[train['abstract'].duplicated()]['abstract'].value_counts()
-    for i in tqdm(train['abstract']):
+    dup = data[data['abstract'].duplicated()]['abstract'].value_counts()
+    for i in tqdm(data['abstract']):
         try:
             abstract_dup.append(dup[i])
         except:
@@ -68,47 +79,47 @@ def get_train_data(data_path = './nytimes-dataset/nytimes.pkl', num_train_data =
     headline_len = []
     headline_dup = []
     headline_kicker = []
-    for i in tqdm(train['headline']):
+    for i in tqdm(data['headline']):
         headline.append(i['main'])
         headline_len.append(len(i['main'].split(' ')))
         headline_kicker.append(i['kicker'])
-    train['headline'] = headline
+    data['headline'] = headline
 
-    dup = train[train['headline'].duplicated()]['headline'].value_counts()
-    for i in tqdm(train['headline']):
+    dup = data[data['headline'].duplicated()]['headline'].value_counts()
+    for i in tqdm(data['headline']):
         try:
             headline_dup.append(dup[i])
         except:
             headline_dup.append(0)
 
     print('Proccess the lead_paragraph')
-    len_lead = [len(i.split(' ')) for i in tqdm(train['lead_paragraph'])]
+    len_lead = [len(i.split(' ')) for i in tqdm(data['lead_paragraph'])]
 
     print('Proccess the keywords')
     keywords = []
-    for i in tqdm(train['keywords']):
+    for i in tqdm(data['keywords']):
         keywords.append([k['value'] for k in i])
     
     print('Proccess the authors')
     authors = []
-    for i in tqdm(train['byline']):
+    for i in tqdm(data['byline']):
         try :
             authors.append([p['firstname'] + ' ' + p['middlename'] + ' ' + p['lastname'] for p in i['person']])
         except:
             authors.append([p['firstname'] + ' ' + p['lastname'] for p in i['person']])
 
-    train = train.drop(columns=['abstract', 'snippet', 'lead_paragraph', 'source', 'pub_date',\
+    data = data.drop(columns=['abstract', 'snippet', 'lead_paragraph', 'source', 'pub_date',\
         'multimedia', 'headline', 'keywords','document_type', 'byline', '_id', 'uri'])
-    train = train.assign(pub_year = pub_year, pub_month = pub_month, pub_day = pub_day, pub_weekday = pub_weekday, pub_time = pub_time, \
+    data = data.assign(pub_year = pub_year, pub_month = pub_month, pub_day = pub_day, pub_weekday = pub_weekday, pub_time = pub_time, \
         abstract = abstract,  abstract_len  = abstract_len,  abstract_dup  = abstract_dup, headline  = headline, headline_len  = headline_len,  \
         headline_dup  = headline_dup,  headline_kicker  = headline_kicker, len_lead  = len_lead,  keywords  = keywords,  authors  = authors)
 
-    return train
+    return data
 
-def get_comment_num(train):
+def get_comment_num(data):
     
     print('Proccess the comment numbers')
-    urls = train['web_url']
+    urls = data['web_url']
     n_comment = []
     for i in tqdm(range(len(urls))):
         
@@ -125,21 +136,26 @@ def get_comment_num(train):
                 ).json()['results']['totalParentCommentsFound'])
         except:
             n_comment.append(-1)
-    train = train.assign(n_comment= n_comment)
-    # train.drop(train.iloc(train['n_comment'] == -1))
+    data = data.assign(n_comment= n_comment)
+    # data.drop(data.iloc(data['n_comment'] == -1))
 
-    return train
+    return data
 
 if __name__ == '__main__':
-    data_src = 'nytimes'
-    data_dst = 'train_v2'
+    data_src = 'article'
+    train_dst = 'train'
+    test_dst = 'test'
     data_path = f'./nytimes-dataset/{data_src}.pkl'
-    train_data_path = f'./nytimes-dataset/{data_dst}.pkl'
+    train_data_path = f'./nytimes-dataset/{train_dst}.pkl'
+    test_data_path = f'./nytimes-dataset/{test_dst}.pkl'
     #get_data()
     
-    train = get_train_data(data_path, 40000)
+    train, test = get_train_data(data_path)
 
     #train = pd.read_pickle(train_data_path)
-    train = get_comment_num(train)
+    #test = pd.read_pickle(test_data_path)
+    #train = get_comment_num(train)
+    #test = get_comment_num(test)
 
     train.to_pickle(train_data_path)
+    test.to_pickle(test_data_path)
